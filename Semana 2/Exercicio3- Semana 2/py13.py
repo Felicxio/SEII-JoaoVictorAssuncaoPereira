81e4ab6313164b9937c316d1dfe9a8ea8301c72d
João Victor Assunção Pereira <105755362+Felicxio@users.noreply.github.com>
2024-01-24 20:39:44 -0300

    Add files via upload

---
+++ Semana 2/Exercicio3- Semana 2/py13.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+print(1 in numbers)
+print(len(numbers))
+numbers.insert(0, -1)
+numbers.clear()
+print(numbers)
---
+++ Semana 2/Exercicio3- Semana 2/py15.py
@@ -0,0 +1,3 @@
+
+for number in range(5, 10, 2):
+    print(number)
